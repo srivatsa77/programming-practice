afc7166963a8cb350cceb9d5e2f64e3f758bb1b2
srivatsa77 <52069077+srivatsa77@users.noreply.github.com>
2020-05-27 23:31:04 +0530

    Test

--- src/sri/io/Main.java
+++ src/sri/io/Main.java
@@ -3,7 +3,7 @@ package sri.io;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("hello world");
+	System.out.println("hello world Srinivas");
         // write your code here
     }
 }
