d49ad11bb1a03a28f1c8e1760f7bf4f40f34dcda
srivatsa77 <52069077+srivatsa77@users.noreply.github.com>
2020-06-08 22:00:09 +0530

    palindrome..

--- src/sri/io/Palindrome.java
+++ src/sri/io/Palindrome.java
@@ -4,9 +4,13 @@ package sri.io;
 public class Palindrome {
     public boolean isPalindrome(String name) //2)name recieved
     {
-        StringBuilder sb = new StringBuilder(name);
-        sb.reverse();
-        return sb.equals(name);
+        String reverse = "";
+       // int len = name.length() - 1;
+        for (int i = name.length()-1; i>=0; i--) {
+            reverse = reverse + name.charAt(i);
+
+        }
+        return reverse == name;
     }
 
     public static void main(String[] args) {
@@ -14,6 +18,6 @@ public class Palindrome {
         Palindrome pal = new Palindrome();
         boolean
                 x = pal.isPalindrome(name); //1)send name as string
-        System.out.print("is " +name + " a palindrome ?  \nAns:- " + x);
+        System.out.print("is " + name + " a palindrome ?  \nAns:- " + x);
     }
 }
