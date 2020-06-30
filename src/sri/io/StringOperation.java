28836da95197e290f96c143e61ee792925b446ef
srivatsa77 <52069077+srivatsa77@users.noreply.github.com>
2020-06-30 21:33:20 +0530

    m1 to find dupe char

--- src/sri/io/StringOperation.java
+++ src/sri/io/StringOperation.java
@@ -1,24 +1,23 @@
 package sri.io;
 
 public class StringOperation {
-    boolean solve(String name) {
-        int z = name.length() - 1;
-        for (int i = 1; i <= z; i++) {
-            for (int j = i-1; j >=0 ; j--) {
-                if(name.charAt(i)!=name.charAt(j))
-                    continue;
-                else
-                    break;
+    boolean hasDupe(String name) {
+
+        for (int i = 1; i < name.length(); i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (name.charAt(i) == name.charAt(j))
+                    return true;
             }
         }
-return
+        return false;
     }
 
+
     public static void main(String[] args) {
-        String name = "abcd";
         StringOperation x = new StringOperation();
-        boolean y = x.solve(name);
-        System.out.print(y);
+        System.out.println(x.hasDupe("abca"));
+        System.out.println(x.hasDupe("abcd"));
+        System.out.println(x.hasDupe("cdefgad"));
 
     }
 }
